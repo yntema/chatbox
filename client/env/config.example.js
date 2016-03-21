07beaafd215d97eec623d6b77cf3e985fe8784bc
Gerrit <gerrityntema@gmail.com>
2016-03-20 21:47:34 -0700

    Clean up files

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -1,18 +1,23 @@
-// YOU DO NOT NEED TO EDIT this code.
-//
-// All this is doing is inserting the parse API keys into every $.ajax
-// request that you make so you don't have to.
+var enteredUser;
 if (!/(&|\?)username=/.test(window.location.search)) {
   var newSearch = window.location.search;
   if (newSearch !== '' & newSearch !== '?') {
     newSearch += '&';
   }
-  newSearch += 'username=' + (prompt('What is your name?') || 'anonymous');
+  enteredUser = prompt('What is your name?');
+  newSearch += 'username=' + (enteredUser || 'anonymous');
   window.location.search = newSearch;
 }
 
-// // Put your parse application keys here!
-// $.ajaxPrefilter(function (settings, _, jqXHR) {
-//   jqXHR.setRequestHeader("X-Parse-Application-Id", "PARSE_APP_ID");
-//   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "PARSE_API_KEY");
-// });
+$.ajax({
+  url: '/classes/users',
+  type: 'POST',
+  data: JSON.stringify({username: enteredUser}),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('enteredUser data', data);
+  },
+  error: function (data) {
+    console.error('chatterbox: Failed to send user', data);
+  }
+});
