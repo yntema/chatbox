05f505a9811d17814bcef57dbc2d9bf629743f98
Gerrit <gerrityntema@gmail.com>
2016-03-21 08:47:03 -0700

    Clean up models and controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,42 +3,42 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) {
-    	models.messages.get(function(err, results) {
-    		res.send(results);
-    	});
+      models.messages.get(function(err, results) {
+        res.send(results);
+      });
     }, 
     post: function (req, res) {
-    	var params = [req.body.username, req.body.text, req.body.roomname];
-    	models.messages.post(params, function(err, results) {
-		  	res.sendStatus(201);
-    	});
+      var params = [req.body.username, req.body.text, req.body.roomname];
+      models.messages.post(params, function(err, results) {
+        res.sendStatus(201);
+      });
     } 
   },
 
   users: {
     get: function (req, res) {
-    	models.users.get(function(err, results) {
-	    	res.send(200);
-    	});
+      models.users.get(function(err, results) {
+        res.send(200);
+      });
     },
     post: function (req, res) {
-    	var params = req.body.username;
-    	models.users.post(params, function(err, results) {
-	    	res.sendStatus(201);
-    	});
+      var params = req.body.username;
+      models.users.post(params, function(err, results) {
+        res.sendStatus(201);
+      });
     }
   },
 
   rooms: {
     get: function (req, res) {
       models.rooms.get(function(err, results) {
-          res.send(200);
+        res.send(200);
       });
     },
     post: function (req, res) {
       var params = req.body.roomname;
       models.rooms.post(params, function(err, results) {
-          res.sendStatus(201);
+        res.sendStatus(201);
       });
     }
   }
--- server/models/index.js
+++ server/models/index.js
@@ -3,30 +3,29 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function (callback) {
-    	db.query('select messages.text, users.name from messages left outer join users on (messages.userId = users.id)', function(err, results) {
-    		callback(err, results);
-    	});
+      db.query('select messages.text, users.name from messages left outer join users on (messages.userId = users.id)', function(err, results) {
+        callback(err, results);
+      });
     }, 
     post: function (params, callback) {
       // var dbArray = [params.body.username,params.body.text,params.body.roomname];
-  		db.query('insert into rooms (name) values (?)', params[2]);
-    	db.query('insert into messages (text, userId) values (?, (select id from users where name = ? limit 1))', [params[1],params[0]], function(err, results) {
-    		callback(err, results);
-    	});
-
+      db.query('insert into rooms (name) values (?)', params[2]);
+      db.query('insert into messages (text, userId) values (?, (select id from users where name = ? limit 1))', [params[1], params[0]], function(err, results) {
+        callback(err, results);
+      });
     } 
   },
 
   users: {
     get: function (callback) {
-    	db.query('select name from users', function(err, result) {
-    		callback(err, results);
-    	})
+      db.query('select name from users', function(err, result) {
+        callback(err, results);
+      });
     },
     post: function (params, callback) {
-    	db.query('insert into users (name) value (?)', params, function(err, results) {
-    		callback(err, results);
-    	})
+      db.query('insert into users (name) value (?)', params, function(err, results) {
+        callback(err, results);
+      });
     }
   },
 
@@ -34,12 +33,12 @@ module.exports = {
     get: function (callback) {
       db.query('select name from rooms', function(err, result) {
         callback(err, results);
-      })
+      });
     },
     post: function (params, callback) {
       db.query('insert into rooms (name) value (?)', params, function(err, results) {
         callback(err, results);
-      })
+      });
     }
   }
 };
